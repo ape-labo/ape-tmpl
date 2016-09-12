a3775992481e8c7880ce5289590bfe2b42e19e75
Taka Okunishi <okunishinishi@gmail.com>
2016-09-12 09:21:43 +0900

    Version incremented to 5.0.19

--- ci/shim.js
+++ ci/shim.js
@@ -9,11 +9,21 @@
 process.chdir(`${__dirname}/..`)
 
 const { runTasks } = require('ape-tasking')
+const aglob = require('aglob')
+const co = require('co')
+const filecopy = require('filecopy')
 const ababelES2015 = require('ababel-es2015')
 
 runTasks('shim', [
   () => ababelES2015('**/*.js', {
     cwd: 'lib',
     out: 'shim/node'
+  }),
+  () => co(function * () {
+    for (let filename of yield aglob('asset/hbs/*.hbs')) {
+      yield filecopy(filename, `shim/${filename}`, {
+        mkdirp: true
+      })
+    }
   })
 ], true)
